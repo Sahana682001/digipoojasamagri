f79420f4816a5e86fb76eaf6bbcc5c1ab9a23d9c
Sahana682001 <sahana.digiworq6@gmail.com>
2025-06-20 11:20:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -80,4 +80,4 @@ app.post('/stripe-webhook', express.raw({type: 'application/json'}), async (req,
   res.json({received: true});
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0',  () => console.log(`Server running on port ${PORT}`));
